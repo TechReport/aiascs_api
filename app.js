80b1c48f880b2cff41485d1f8f1b49092c4ff4ba
Kijacode <kijadanford@gmail.com>
2021-03-22 22:20:46 +0300

    refactor: clearn unused code

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app =express();
 const morgan =require("morgan");
 const {urlencoded,json} = require("body-parser");
 const cors =require("cors");
-const qroc = require("./utils/qrcode_generator");
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(urlencoded({extended:true}));
 app.use(json());
 
-// qroc("jhkjhfkdjf","kija")
+
 ///hare attache routes,middlware for rsourcess 
 app.use('/aiascs',//rotest +middleware
 );
