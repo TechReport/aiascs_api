18ee127f3dcba4ebc676b3a33f31a9ab6bf234c8
Kijacode <kijadanford@gmail.com>
2021-03-31 21:11:31 +0300

    refactor notification

--- index.js
+++ index.js
@@ -5,41 +5,43 @@ const socketServer = require("./utils/notification/nofication_setup");
 const http = require("http");
 
 
-app.set("port", ENV.PORT);
+// app.set("port", ENV.PORT);
 const server = http.createServer(app);
-const io = require('socket.io')(server)
+// const io = require('socket.io')(server)
+
 const connect = () => {
   return mongoose.connect(ENV.DB_URL)
 }
 
 
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => setNotificationEvent(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
-});
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => setNotificationEvent(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
 
-const setNotificationEvent =async (socket) => {
-  print("in  set noti");
-      // Emitting a new message. Will be consumed by the client
-      socket.emit("product","Kijacode");
-    };
-server.listen(ENV.PORT,()=>{
-  console.log("Listening to PORT :"+ ENV.PORT);
-});
-// connect()
-//     .then(async (connection) => {
-//         server.listen(ENV.PORT,()=>{
-//           console.log("Listening to PORT :"+ ENV.PORT);
-//         });
-//     })
-//     .catch((e) => {
-//         console.log('error  DB/SERVER : ' + e)
-//     })
+// const setNotificationEvent =async (socket) => {
+//   print("in  set noti");
+//       // Emitting a new message. Will be consumed by the client
+//       socket.emit("product","Kijacode");
+//     };
+// server.listen(ENV.PORT,()=>{
+//   console.log("Listening to PORT :"+ ENV.PORT);
+// });
+connect()
+    .then(async (connection) => {
+        server.listen(ENV.PORT,()=>{
+          console.log("Listening to PORT :"+ ENV.PORT);
+          socketServer(server);
+        });
+    })
+    .catch((e) => {
+        console.log('error  DB/SERVER : ' + e)
+    })
