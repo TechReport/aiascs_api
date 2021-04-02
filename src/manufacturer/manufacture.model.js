b0bd6592ccbb3688078f54f44abfebaad7f976c8
Kijacode <kijadanford@gmail.com>
2021-04-02 18:32:35 +0300

    feat: add manufacture model => relationship with product agent

---
+++ src/manufacturer/manufacture.model.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const manufacture = new mongoose.Schema({
+    regno:{
+        type:String,
+        unique:true,
+        required:true
+    },
+
+    name:{
+    type:String,
+    min:5,
+    unique:true,
+    required:true
+},
+
+    phonenumber: {
+type:Number,
+max:12,
+min:12,
+unique:true,
+required:true
+},
+email:{
+type:String,
+required:true
+},
+
+location:{
+    country: String,
+    district: String,
+    ward: String,
+},
+createdAt:{
+    type:Date,
+    required:true,
+    default:Date.now()
+},
+
+productAgent:[
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'productAgent',
+  
+    }
+]
+
+
+});
+
+
+manufacture.pre("save",function(next){
+    if((this.email).contain("@"))
+    {
+        next();
+    }
+    next (new Error("Enter valid Email"));
+});
+
+
+
+manufacture.post("remove",async (doc,next)=>{
+_.forEach(doc.productAgent,(singlemanufactureDoc)=>{
+await productAgent.findOneAndUpdate({_id:singlemanufactureDoc._id},{$pullAll:{manufacture:[doc._id]}}
+    );
+}).exec();
+
+next();
+
+});
+
+
+
+module.exports = mongoose.model("manufacture",manufacture);
