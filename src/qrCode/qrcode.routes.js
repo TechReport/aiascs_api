091b1518342d0cb850b331bc1ee08fdc2a2276e6
dae54 <amethysternest@gmail.com>
2021-04-04 03:49:40 +0300

    feat: add qr code controlling logics

---
+++ src/qrCode/qrcode.routes.js
@@ -0,0 +1,8 @@
+const Router = require('express')
+const router = new Router()
+const qrcodeController = require('./qrcode.controller')
+// const sessionMonitor = require('../../utils/middlewares/sessionMonitor')
+
+router.post('/', )
+
+module.exports = router
