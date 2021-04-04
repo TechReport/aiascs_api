ec8ab8ededdc6ae9062472cc7cb21fca3268da75
dae54 <amethysternest@gmail.com>
2021-04-04 03:44:52 +0300

    feat: create register product component

--- src/agro_inputs/products.controller.js
+++ src/agro_inputs/products.controller.js
@@ -1,28 +1,68 @@
 const Products = require('./products.modal')
-
+const QRCodeModel = require('../qrCode/qrcode.model')
+const QRCodeController = require('../qrCode/qrcode.controller')
 
 
 module.exports = {
     register: async (req, res) => {
+        async function saveProductData(qrcodeIds) {
+            console.log('qrcodeIds', qrcodeIds)
+            let productData = []
+            for (var i = 0; i < req.body.newProduct.count; i++) {
+                productData.push({
+                    ...req.body.newProduct,
+                    companyId: req.body.companyId,
+                    qrcode: qrcodeIds[i]
+                })
+            }
+            // console.log('mark issued')
+            qrcodeIds.forEach(id => {
+                QRCodeController.markIssued(id)
+            })
+            // console.log('this aint blocking')
 
-        console.log(req.body)
-
-        // const newProduct = req.body.newProduct
-
-        for (let i = 0; i <= req.body.newProduct.count; i++) {
-            let newProduct = await Products.create(req.body.newProduct)
-
+            await Products.insertMany(productData, { rawResult: true })
+                .then(response => {
+                    console.log(response)
+                    console.log('products created')
+                    res.status(200).json({
+                        message: `'${response.insertedCount}' products have been created successfully`,
+                        data: {
+                            status: response.result.ok,
+                            productsCreatedCount: response.insertedCount,
+                            products: response.ops
+                        }
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
         }
+        var unusedQRCodes = await QRCodeModel.find({ status: 0 }, '_id', { limit: Number(req.body.newProduct.count) })
 
+        if (unusedQRCodes.length < req.body.newProduct.count) {
+            await QRCodeController.generateQRCode(req.body.newProduct.count - unusedQRCodes.length)
+                .then(res => {
+                    let idsForCreatedQRCode = Object.values(res)
+                    let previousPresentQRCodeIds = unusedQRCodes.map(code => code._id)
 
-
-
-        // res.render();
-
-
-        res.status(200).json({
-            message: 'successfully created',
-            data: ''
-        })
+                    let allQRCodes = idsForCreatedQRCode.concat(previousPresentQRCodeIds)
+                    saveProductData(allQRCodes)
+                })
+        } else {
+            saveProductData(unusedQRCodes.map(code => code._id))
+        }
+    },
+    getAll: async (req, res, next) => {
+        try {
+            const products = await Products.find().populate('qrcode').sort('-createdAt').exec()
+            res.status(200).json({
+                message: 'Products loaded successfully',
+                data: products
+            })
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
     }
 }
--- src/agro_inputs/products.modal.js
+++ src/agro_inputs/products.modal.js
@@ -1,30 +1,47 @@
 const mongoose = require('mongoose')
 
-let agroInputsSchema = mongoose.Schema({
+let productsSchema = mongoose.Schema({
     name: {
         type: String,
         trim: true,
         required: true
     },
-    batchId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    },
-    qrCodeRef: {
+    photoInfo: {
         type: String,
-        unique: true,
+        trim: true
     },
-    qrCodeData: {
-        type: Object,
-        // unique: true,
-        // required: true,
+    isRevoked: {
+        type: Boolean,
+        default: false
+    },
+    qrcode: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'qrcode',
+        unique: true
     },
     expiry: {
         type: mongoose.Schema.Types.Date,
         trim: true,
+        required: true
     },
+    companyId: {
+        type: String,
+        // type: mongoose.Schema.Types.ObjectId,
+        // ref: 'manufacturingCompany',
+    }
 },
     { timestamps: true }
 )
 
-module.exports = mongoose.model('agroInputs', agroInputsSchema)
+productsSchema.virtual('batchInfo').get(function () {
+    console.log('i am called')
+    return this.createdAt
+})
+
+productsSchema.virtual('hasExpired').get(function () {
+    console.log('i am called')
+    return this.expiry > Date.now() ? true : false
+})
+
+
+module.exports = mongoose.model('agroInputs', productsSchema)
