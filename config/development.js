6a99959d4915cb0b10bf15380a0bf7a362da58a3
dae54 <amethysternest@gmail.com>
2021-04-04 15:40:20 +0300

    deprec: mark env exporter files as deprecated

--- config/development.js
+++ config/development.js
@@ -1,8 +1,12 @@
+/**
+ * @deprecated it will be deleted in the later release
+ * @author daniel ernest
+ */
 module.exports = {
-    CLOUD_NAME: '',    
-    API_KEY_CLOUDINARY: '',  
-    API_SECRET_CLOUDINARY: '',
-    CLOUDINARY_ENV_VARIABLE: '',  
-    DB_URL :  'mongodb://localhost:27017/aiascs',
-    PORT: 5501,
-  }
+  CLOUD_NAME: '',
+  API_KEY_CLOUDINARY: '',
+  API_SECRET_CLOUDINARY: '',
+  CLOUDINARY_ENV_VARIABLE: '',
+  DB_URL: 'mongodb://localhost:27017/aiascs',
+  PORT: 5501,
+}
