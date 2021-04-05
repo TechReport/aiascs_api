2ed24da0cb912a1e558c6aaa7beb3f8b786c6a76
dae54 <amethysternest@gmail.com>
2021-04-05 21:06:05 +0300

    feat: add signout handler

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -5,6 +5,7 @@ const sessionMonitor = require('../../utils/middlewares/sessionMonitor')
 
 router.get('/', sessionMonitor, userController.getAll)
 router.post('/login', userController.login)
+router.post('/signout', sessionMonitor, userController.signOut)
 router.post('/register', sessionMonitor, userController.register)
 router.patch('/resetPassword', sessionMonitor, userController.resetPassword)
 
