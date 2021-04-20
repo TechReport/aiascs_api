e71c06da027183a7a7a45f83e32a7eca07eaadd4
dae54 <amethysternest@gmail.com>
2021-04-21 00:53:43 +0300

    manufacturer

--- src/manufacturer/manufacture.controller.js
+++ src/manufacturer/manufacture.controller.js
@@ -2,66 +2,94 @@ const { isNull } = require('lodash');
 const Manufacture = require('./manufacture.model');
 
 module.exports = {
-  getManufuctureById:async (req, res, next) =>{
-  - await Manufacture.findById(req.manufactureId,(error,doc)=>{
-      if(error)
-      {
-        next(error)
-      }
-    res.status(200).json(doc);
-    }).populate("productAgent"),exec();
-     
+    getManufuctureById: async (req, res, next) => {
+        - await Manufacture.findById(req.manufactureId, (error, doc) => {
+            if (error) {
+                next(error)
+            }
+            res.status(200).json(doc);
+        }).populate("productAgent"), exec();
+
 
     },
-  
-  getAllManufacture :async (req, res, next) => {
-  await   Manufacture.find({},(error,manufactures)=>{
-    if(error)
-    {
-      next(error)
-    }
-  res.status(200).json(manufactures);
-  }).populate("productAgent").
-  sort('-createdAt').
-  lean().
-  exec();
- 
-  },
 
-  createManufacture: async (req, res, next) => {
-    const manufactures = await Manufacture.create(req.body)
-    res.status(201).json(manufactures.toJSON())
-  },
+    getAllManufacture: async (req, res, next) => {
+        try {
+            const manufactures = await Manufacture.find()
+                // .populate("admin")
+                // .populate('roleId', 'name')
+                .populate({
+                    path: 'admin',
+                    populate: [
+                        { path: 'role', select: 'name' },
+                    ],
+                })
+                .populate("productAgent")
+                .sort('-createdAt')
+                .lean()
+                .exec()
+            res.status(200).json(manufactures);
 
-
-  removeManufactureyId: async (req, res, next) => {
-   await Manufacture.findByIdAndDelete(req.manufactureId).exec();
-   res.status(202).json({
-    "message":"sucess fully deleted"
-   });
-  },
-  updateManufactureById: async (req, res, next) => {
-    const update = req.body;
-    return  await Manufacture.findByIdAndUpdate(req.manufactureId, update, { new: true },(error,updatedManufacture)=>{
-      if(error)
-      {
-        next(error)
-      }
-      res.status(204).json(updatedManufacture);
-    },{ new: true }).exec();
-  },
-  addProductAgentToManufacture:async (req, res, next) => {
-    const productAgents = req.body;
-
-  let manufactures = await  Manufacture.findByIdAndUpdate( req.manufactureId,{
-      $push:{
-        productAgent:{
-          $each:productAgents
+        } catch (err) {
+            console.log(err)
+            next(err)
         }
-      }
-    } , { new: true }).exec();
+    },
 
-    res.status(201).json(manufactures);
-  },
+    createManufacture: async (req, res, next) => {
+        console.log(req.body)
+        const manufactures = await Manufacture.create(req.body)
+        res.status(201).json(manufactures.toJSON())
+    },
+
+
+    removeManufactureyId: async (req, res, next) => {
+        await Manufacture.findByIdAndDelete(req.manufactureId).exec();
+        res.status(202).json({
+            "message": "sucess fully deleted"
+        });
+    },
+    updateManufactureById: async (req, res, next) => {
+        const update = req.body;
+        return await Manufacture.findByIdAndUpdate(req.manufactureId, update, { new: true }, (error, updatedManufacture) => {
+            if (error) {
+                next(error)
+            }
+            res.status(204).json(updatedManufacture);
+        }, { new: true }).exec();
+    },
+    addProductAgentToManufacture: async (req, res, next) => {
+        const productAgents = req.body;
+
+        let manufactures = await Manufacture.findByIdAndUpdate(req.manufactureId, {
+            $push: {
+                productAgent: {
+                    $each: productAgents
+                }
+            }
+        }, { new: true }).exec();
+
+        res.status(201).json(manufactures);
+    },
+    assignAdmin: async (req, res, next) => {
+        console.log(req.body)
+        console.log(req.params)
+        const { companyId, adminId } = req.params
+
+        await Manufacture.findByIdAndUpdate(companyId, { admin: adminId }, { new: true, useFindAndModify: false })
+            .populate({
+                path: 'admin',
+                populate: [
+                    { path: 'role', select: 'name' },
+                ],
+            })
+            .then(updatedManufacturer => {
+                console.log(updatedManufacturer)
+                res.status(201).json(updatedManufacturer);
+            }).catch(err => {
+                console.log(err)
+                next(err)
+            })
+    }
 };
 
