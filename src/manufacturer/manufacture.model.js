f0f5636593a143e71131968a6366a0d7f4cc6872
Kijacode <kijadanford@gmail.com>
2021-04-21 12:18:42 +0300

    feat:bumb eslint errors with prettier

--- src/manufacturer/manufacture.model.js
+++ src/manufacturer/manufacture.model.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const _ = require("lodash");
-const ProductAgent = require("../product_agent/product_agent.model");
-const Product = require("../agro_inputs/products.modal");
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const ProductAgent = require('../product_agent/product_agent.model');
+const Product = require('../agro_inputs/products.modal');
 
-<<<<<<< HEAD
 const manufacture = new mongoose.Schema(
   {
     regno: {
@@ -28,8 +28,8 @@ const manufacture = new mongoose.Schema(
       required: true,
       unique: true,
       match: [
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid email",
+        /^(([^<>()[\]\\.,;:\s@\\"]+(\.[^<>()[\]\\.,;:\s@\\"]+)*)|(\\".+\\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please enter a valid email',
       ],
     },
 
@@ -47,95 +47,34 @@ const manufacture = new mongoose.Schema(
     productAgent: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "productAgent",
+        ref: 'productAgent',
       },
     ],
   },
-  { timestamps: true }
+  // eslint-disable-next-line prettier/prettier
+  { timestamps: true },
 );
 
-manufacture.pre("save", function (next) {
+manufacture.pre('save', (next) => {
   if (true) {
     next();
   }
-  next(new Error("Enter valid Email"));
-});
-
-manufacture.post("remove", async (doc, next) => {
-  _.forEach(doc.productAgent, (singlemanufactureDoc) => {
-    ProductAgent.findOneAndUpdate(
-      { _id: singlemanufactureDoc._id },
-      { $pullAll: { manufacture: [doc._id] } }
-    );
-  }).exec();
-=======
-const manufacture = new mongoose.Schema({
-    regno: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    name: {
-        type: String,
-        min: 5,
-        unique: true,
-        required: true,
-    },
-
-    phonenumber: {
-        type: Number,
-        unique: true,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
-    },
-    admin: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users',
-    },
-    location: {
-        country: String,
-        district: String,
-        ward: String,
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now(),
-    },
-
-    productAgent: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'productAgent',
-        },
-    ],
-}, { timestamps: true });
-
-manufacture.pre('save', function (next) {
-    if (true) {
-        next();
-    }
-    next(new Error('Enter valid Email'));
+  next(new Error('Enter valid Email'));
 });
 
 manufacture.post('remove', async (doc, next) => {
-    _.forEach(doc.productAgent, (singlemanufactureDoc) => {
-        ProductAgent.findOneAndUpdate(
-            { _id: singlemanufactureDoc._id },
-            { $pullAll: { manufacture: [doc._id] } }
-        );
-    }).exec();
->>>>>>> 72444d530de0350961f56421e32895bde0f6d7aa
+  _.forEach(doc.productAgent, (singlemanufactureDoc) => {
+    ProductAgent.findOneAndUpdate(
+      { _id: singlemanufactureDoc._id },
+      // eslint-disable-next-line prettier/prettier
+      { $pullAll: { manufacture: [doc._id] } },
+    );
+  }).exec();
 
-    next();
+  next();
 });
 
-manufacture.post("remove", async (doc, next) => {
+manufacture.post('remove', async (doc, next) => {
   Product.deleteMany({ manufacture: doc._id }, (error, response) => {
     if (error) {
       next(error);
@@ -145,4 +84,4 @@ manufacture.post("remove", async (doc, next) => {
   });
 });
 
-module.exports = mongoose.model("manufacture", manufacture);
+module.exports = mongoose.model('manufacture', manufacture);
