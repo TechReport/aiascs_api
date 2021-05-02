3044bcb3c3654e896846bb0709ca91eadd38f56c
dae54 <amethysternest@gmail.com>
2021-05-02 12:56:05 +0300

    refactor

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -4,10 +4,13 @@ const router = new Router();
 const userController = require('./user.controller');
 const sessionMonitor = require('../../utils/middlewares/sessionMonitor');
 
-router.get('/', sessionMonitor, userController.getAll)
-router.get('/role', sessionMonitor, userController.getUsersByRole)
 
+router.get('/role', userController.getUsersByRole)
+router.get('/getCompanyUserByRole', userController.getCompanyUserByRole)
 router.get('/:id', sessionMonitor, userController.getById)
+
+router.get('/', sessionMonitor, userController.getAll)
+
 router.post('/login', userController.login)
 router.post('/signout', sessionMonitor, userController.signOut)
 router.post('/register', sessionMonitor, userController.register)
