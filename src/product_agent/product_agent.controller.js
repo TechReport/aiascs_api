4ca2bb978cc3004204527db9010020820cf6b71e
Kijacode <kijadanford@gmail.com>
2021-05-04 11:31:25 +0300

    fix: on get product route

--- src/product_agent/product_agent.controller.js
+++ src/product_agent/product_agent.controller.js
@@ -1,61 +1,70 @@
+/* eslint-disable prettier/prettier */
 const ProductAgent = require('./product_agent.model');
 
 module.exports = {
-  getProductAgentById:async (req, res, next) =>{
- await ProductAgent.findById(req.productAgentId,(error,productAgent)=>{
-      if(error)
-      {
-        next(error)
+  getProductAgentById: async (req, res, next) => {
+    await ProductAgent.findById(req.productAgentId, (error, productAgent) => {
+      if (error) {
+        next(error);
       }
-    res.status(200).json(productAgent);
+      res.status(200).json(productAgent);
     }).exec();
-    },
-  
-  getAllProductAgent :async (req, res, next) => {
- await   ProductAgent.find({},(error,productAgent)=>{
-    if(error)
-    {
-      next(error)
-    }
-  res.status(200).json(productAgent);
-  }).populate("manufacture").
-  sort('-createdAt').
-  lean().
-  exec();
   },
 
-  createProductAgent: async (req, res, next) => {
-    const productAgents = await ProductAgent.create(req.body)
-    res.status(201).json(productAgents.toJSON())
+  getAllProductAgent: async (req, res, next) => {
+    await ProductAgent.find({}, (error, productAgent) => {
+      if (error) {
+        next(error);
+      }
+      res.status(200).json(productAgent);
+    })
+      .populate('manufacture')
+      .sort('-createdAt')
+      .lean()
+      .exec();
   },
 
-
-  removeProductAgentId: async (req, res, next) => {
-   await  ProductAgent.findByIdAndDelete(req.productAgentId).exec();
-   res.status(202).json({
-    "message":"sucess fully deleted"
-   });
+  createProductAgent: async (req, res) => {
+    const productAgents = await ProductAgent.create(req.body);
+    res.status(201).json(productAgents.toJSON());
   },
-  updateProductAgentById:async (req, res, next) => {
+
+  removeProductAgentId: async (req, res) => {
+    await ProductAgent.findByIdAndDelete(req.productAgentId).exec();
+    res.status(202).json({
+      message: 'sucess fully deleted',
+    });
+  },
+  updateProductAgentById: async (req, res, next) => {
     const update = req.body;
-    return await ProductAgent.findByIdAndUpdate(req.productAgentId, update, { new: true },(error,updatedProductAgent)=>{
-      if(error)
-      {
-        next(error)
-      }
-      res.status(204).json(updatedProductAgent);
-    }, { new: true }).exec();
-  },
-  addManufactureToProductAgent:async (req, res, next) => {
-    const manufacture = req.body;
-    let productAgent = await ProductAgent.findByIdAndUpdate( req.productAgentId,{
-      $push:{
-        productAgent:{
-          $each:manufacture
+    // eslint-disable-next-line no-return-await
+    return await ProductAgent.findByIdAndUpdate(
+      req.productAgentId,
+      update,
+      { new: true },
+      (error, updatedProductAgent) => {
+        if (error) {
+          next(error);
         }
-      }
-    }, { new: true }).exec();
+        res.status(204).json(updatedProductAgent);
+      },
+      { new: true },
+    ).exec();
+  },
+  addManufactureToProductAgent: async (req, res) => {
+    const manufacture = req.body;
+    const productAgent = await ProductAgent.findByIdAndUpdate(
+      req.productAgentId,
+      {
+        $push: {
+          productAgent: {
+            $each: manufacture,
+          },
+        },
+      },
+      // eslint-disable-next-line comma-dangle
+      { new: true }
+    ).exec();
     res.status(201).json(productAgent);
   },
 };
-
