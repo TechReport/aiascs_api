fb684dda3246e35e78005b33c041e97d851b15bb
dae54 <amethysternest@gmail.com>
2021-05-07 00:18:14 +0300

    refactor

--- src/quality_controller/qualit_controller.routes.js
+++ src/quality_controller/qualit_controller.routes.js
@@ -7,7 +7,7 @@ const sessionMonitor = require('../../utils/middlewares/sessionMonitor');
 router.get('/', sessionMonitor, qualityControlController.getAll)
 router.get('/:id', sessionMonitor, qualityControlController.getById)
 
-router.post('/', sessionMonitor, qualityControlController.register)
+router.post('/register', sessionMonitor, qualityControlController.register)
 router.delete('/', sessionMonitor, qualityControlController.delete)
 router.patch('/update/:id', sessionMonitor, qualityControlController.update)
 router.put('/assignAdmin/:companyId/:adminId', sessionMonitor, qualityControlController.assignAdmin)
