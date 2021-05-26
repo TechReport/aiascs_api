0aae497257aff18da1eb6ada5b5a597c3c2931e8
dae54 <amethysternest@gmail.com>
2021-05-27 01:02:51 +0300

    feat: add location

--- src/users/user.modal.js
+++ src/users/user.modal.js
@@ -2,101 +2,93 @@ const mongoose = require('mongoose');
 const manufactureModel = require('../manufacturer/manufacture.model');
 
 const userSchema = mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            trim: true,
-            required: true,
-            dropDups: true,
-        },
-        phoneNumber: {
-            type: String,
-            unique: true,
-            trim: true,
-            required: true,
-            dropDups: true,
-        },
-        country: {
-            type: String,
-            trim: true,
-        },
-        region: {
-            type: String,
-            trim: true,
-        },
-        district: {
-            type: String,
-            trim: true,
-        },
-        ward: {
-            type: String,
-            trim: true,
-            // required: true,
-        },
-        gender: {
-            type: String,
-            enum: ['male', 'female'],
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            select: false,
-        },
-        firstTimeLoginStatus: {
-            type: Number,
-            enum: [0, 1],
-            default: 0,
-        },
-        companyId: {
-            type: mongoose.Schema.Types.ObjectId,
-            // required: true,
-            refPath: 'onModel'
-        },
-        onModel: {
-            type: String,
-            // required: true,
-            enum: ['manufacture', 'productAgent', 'qualityController']
-        },
-        /*
-         *for first login, before user changes password, firstTimeLoginStats is set to 0
-         *after resetting password, the firstTimeLoginStats is set to 1
-         */
-        role: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: 'role',
-        },
-        emailVerified: {
-            type: Boolean,
-            default: false,
-        },
-        token: {
-            type: String,
-        },
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    { timestamps: true }
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      dropDups: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      dropDups: true,
+    },
+    location: {
+      country: { type: String, default: 'Tanzania' },
+      region: String,
+      district: String,
+      ward: String,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    firstTimeLoginStatus: {
+      type: Number,
+      enum: [0, 1],
+      default: 0,
+    },
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      // required: true,
+      refPath: 'onModel',
+    },
+    onModel: {
+      type: String,
+      // required: true,
+      enum: ['manufacture', 'productAgent', 'qualityController'],
+    },
+    /*
+     *for first login, before user changes password, firstTimeLoginStats is set to 0
+     *after resetting password, the firstTimeLoginStats is set to 1
+     */
+    role: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'role',
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    token: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 userSchema.pre('deleteOne', async (next) => {
-    await manufactureModel.findOneAndUpdate({ admin: this._id }, { admin: undefined }, { useFindAndModify: false })
-    next();
+  await manufactureModel.findOneAndUpdate(
+    { admin: this._id },
+    { admin: undefined },
+    { useFindAndModify: false }
+  );
+  next();
 });
 
 userSchema.virtual('fullName').get(function () {
-    return this.firstName + ' ' + this.lastName;
+  return this.firstName + ' ' + this.lastName;
 });
-
 
 module.exports = mongoose.model('users', userSchema);
