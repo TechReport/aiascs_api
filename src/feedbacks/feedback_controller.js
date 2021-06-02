19da0c12300f2ec23450e0a47f9d1c7d9faa4b66
dae54 <amethysternest@gmail.com>
2021-06-02 15:59:06 +0300

    fix: feedback for product verification

--- src/feedbacks/feedback_controller.js
+++ src/feedbacks/feedback_controller.js
@@ -4,9 +4,9 @@
 /* eslint-disable indent */
 /* eslint-disable prettier/prettier */
 const FeedbackModel = require('./feedback_model');
-// const productController = require('../agro_inputs/products.modal');
+const ProductModal = require('../agro_inputs/products.modal');
 
-const productController = require('../manufacturer/manufacture.model');
+// const productController = require('../agro_inputs/products.modal');
 
 module.exports = {
   createFeedback: async (req, res, next) => {
@@ -16,43 +16,57 @@ module.exports = {
 
   validateFromSMS: async (req, res, next) => {
     // const number = req.body.message;
+    console.log(req.body);
 
-    productController
-      .findOne({ token: req.body.message })
+    ProductModal.findOne({ token: req.body.message })
       .populate('companyId')
       .populate('productAgent')
       .lean()
       .exec()
       .then((product) => {
-        console.log(typeof (product.isRevoked));
-        if (product.isRevoked) {
-          const feedback = new FeedbackModel(
-            {
+        console.log(product);
+        // console.log(typeof (product.isRevoked));
+        if (product) {
+          if (product.isRevoked) {
+            const feedback = new FeedbackModel({
               // eslint-disable-next-line quote-props
-              'message': req.body.message,
+              message: req.body.message,
               // eslint-disable-next-line quote-props
-              'fromID': req.body.fromID
-            }
-          );
-          feedback.save();
-          res.status(200).json({
-            message: 'Product is Fake',
-          });
+              fromID: req.body.fromID,
+            });
+            feedback.save();
+            res.status(200).json({
+              message:
+                'Product has been revoked by quality controllers. It is Fake.',
+              from: req.body.fromID,
+            });
+          } else {
+            const feedback = new FeedbackModel({
+              // eslint-disable-next-line quote-props
+              message: req.body.message,
+              // eslint-disable-next-line quote-props
+              fromID: req.body.fromID,
+            });
+            feedback.save();
+            res.status(200).json({
+              message: 'Product is Genuine',
+              from: req.body.fromID,
+            });
+          }
         } else {
-          const feedback = new FeedbackModel(
-            {
-              // eslint-disable-next-line quote-props
-              'message': req.body.message,
-              // eslint-disable-next-line quote-props
-              'fromID': req.body.fromID
-            }
-          );
+          const feedback = new FeedbackModel({
+            // eslint-disable-next-line quote-props
+            message: req.body.message,
+            // eslint-disable-next-line quote-props
+            fromID: req.body.fromID,
+          });
           feedback.save();
           res.status(200).json({
-            message: 'Product is Genuine',
+            message:
+              'Product does Not Exist or it is Not Genuine. Check the token and try again. If the problem persists, report to the nearest quality controllers office, with the package',
+            from: req.body.fromID,
           });
         }
       });
-},
-
-    };
+  },
+};
--- src/feedbacks/feedback_model.js
+++ src/feedbacks/feedback_model.js
@@ -14,7 +14,7 @@ const feedbackSchema = new mongoose.Schema({
 });
 
 feedbackSchema.pre('validate', async function (next) {
-  if (this.fromID.length > 102 && this.message.length > 16) {
+  if (this.fromID.length > 12 && this.message.length > 16) {
     this.feedBackFrom = 'QualityController';
   }
   next();
