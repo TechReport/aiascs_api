7ede92b40ab1de80ec68e04a599380d01810c44a
Kijacode <kijadanford@gmail.com>
2021-06-02 06:10:30 +0300

    feat:add feedback pre hooks on model

--- src/feedbacks/feedback_model.js
+++ src/feedbacks/feedback_model.js
@@ -14,14 +14,14 @@ const feedbackSchema = new mongoose.Schema({
 });
 
 feedbackSchema.pre('validate', async function (next) {
-  if (this.identifier.length > 12 && this.message.length > 16) {
+  if (this.fromID.length > 102 && this.message.length > 16) {
     this.feedBackFrom = 'QualityController';
   }
   next();
 });
 
 feedbackSchema.virtual('messageCategory').get(function () {
-  if (this.identifier.length === 12 && this.message.length === 16) {
+  if (this.fromID.length === 12 && this.message.length === 16) {
     return 'verification';
   }
   return 'feedback';
