e9b2e4eed083c2c449798655de6947c55e1e0182
dae54 <amethysternest@gmail.com>
2021-07-05 14:11:05 +0300

    populate admin data on getting company by id

--- src/quality_controller/quality_controller.controller.js
+++ src/quality_controller/quality_controller.controller.js
@@ -37,6 +37,7 @@ module.exports = {
     console.log(req.params);
     console.log(req.body);
     await QualityController.findById(req.params.id)
+      .populate('admin')
       .then((response) => {
         console.log(response);
         return res.status(200).json(response.toJSON());
@@ -88,5 +89,75 @@ module.exports = {
         console.log(err);
         next(err);
       });
+  },
+  updateLogo: async (req, res) => {
+    try {
+      console.log('am here');
+      const { companyId } = req.params;
+      const formidable = require('formidable');
+      const form = formidable();
+      // eslint-disable-next-line global-require
+      const { cloudinary } = require('../../utils/Cloudinary');
+
+      form.parse(req, async (err, fields, files) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'At least one image is required',
+            developerMessage: 'a product photo is required',
+          });
+        }
+
+        if (!files.file) {
+          return res.status(500).json({
+            message: 'At least one image is required',
+            developerMessage: 'a product photo is required',
+          });
+        }
+
+        const uploadedResponse = await cloudinary.uploader
+          .upload(files.file.path, {
+            upload_preset: 'aiascs',
+            folder: 'logos',
+          })
+          .catch((error) => {
+            console.log('cloudinary error ', error);
+            return res.status(500).json({
+              message: 'Connection to cloudinary failed',
+              developerMessage: error,
+            });
+          });
+        // console.log(uploadedResponse);
+        const mongoose = require('mongoose');
+        //   fields.photo = uploadedResponse;
+        const updatedLogo = await QualityController.findOneAndUpdate(
+          { _id: mongoose.Types.ObjectId(companyId) },
+          { logo: uploadedResponse.url },
+          { new: true, useFindAndModify: false }
+        );
+        console.log(updatedLogo);
+        //   const unregistered = await UnregisteredProducts.create(fields);
+        // console.log(unregistered);
+        return res.status(200).json({
+          message: 'logo added successfully',
+          data: updatedLogo,
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  updatePostalAddress: async (req, res) => {
+    const { companyId } = req.params;
+    const { postalDetails } = req.body;
+    // console.log(postalDetails);
+    // console.log(req.body);
+    await QualityController.findByIdAndUpdate(
+      { _id: companyId },
+      { postalBox: postalDetails },
+      { new: true, useFindAndModify: false }
+    ).then((response) => {
+      console.log(response);
+      return res.status(200).json(response);
+    });
   },
 };
